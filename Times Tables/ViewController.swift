96c59c897a231421afa4de3e10896aa82a22c5df
Justin Vallely <justinvallely@gmail.com>
2015-05-18 23:15:25 -0600

    Elementary Times Tables
    Very basic Swift app for iOS demonstrating how to use tables and
    sliders. Shows elementary times tables from 1-20.

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by Justin Vallely on 5/18/15.
+//  Copyright (c) 2015 JMVapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet var sliderValue: UISlider!
+    
+    @IBOutlet var table: UITableView!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        table.reloadData()
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(sliderValue.value * 20)
+        
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
